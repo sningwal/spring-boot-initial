e9266496ca547e2ea47b19781b8f8fe5237df0a1
sningwal <sandeepningwal5@gmail.com>
2026-06-06 13:24:03 +0530

    whitelisted https://shortly.sandeepningwal.com/

--- src/main/java/com/urlshortner/urlshortner/config/CorsConfig.java
+++ src/main/java/com/urlshortner/urlshortner/config/CorsConfig.java
@@ -16,7 +16,8 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:3000"
+                "http://localhost:3000",
+                "https://shortly.sandeepningwal.com"
         ));
 
         configuration.setAllowedMethods(List.of(
